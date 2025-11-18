d8da2673fff9e726ca6f1efda6dd279b04b7fe0b
vleadadmin <admin@vlabs.ac.in>
2025-11-18 10:14:05 +0000

    https://virtual-labs.github.io/virtual-labs/exp-heap-sort-iiith click on the link to test your code.

---
+++ insert-heap/simulation/js/realization.js
@@ -0,0 +1,86 @@
+var realizationCatalog = {
+
+  // HTML artefacts
+
+  "Demo: Heap Insert": {
+    "reqs_satisfied": "Demo the insertion operation",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/demo_insert.html"
+  },
+  "Practice: Heap Insert": {
+    "reqs_satisfied": "Practice the insertion operation",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/practice_insert.html"
+  },
+  "Exercise: Heap Insert": {
+    "reqs_satisfied": "Test the insertion operation",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/exercise_insert.html"
+  },
+  "Demo: Heap Extract": {
+    "reqs_satisfied": "Demo the Extract-max operation",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/demo_extract.html"
+  },
+  "Practice: Heap Extract": {
+    "reqs_satisfied": "Practice the Extract-max operation",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/practice_extract.html"
+  },
+  "Exercise: Heap Extract": {
+    "reqs_satisfied": "Test the Extract-max operation",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/exercise_extract.html"
+  },
+  "Demo: Heap Sort": {
+    "reqs_satisfied": "A full demo showing the whole Heap Sort Procedure",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/demo_heapsort.html"
+  },
+  "Practice: Heap Sort": {
+    "reqs_satisfied": "A full demo showing the whole Heap Sort Procedure",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/practice_heapsort.html"
+  },
+
+  // Image artefacts
+
+  "Image: MinMax heap Representation": {
+    "resource_type": "image",
+    "url" : "/build/code/static/img/minmax_heap.png"
+  },
+  "Image: Insert Heap Representation": {
+    "resource_type": "image",
+    "url": "/build/code/static/img/insert.png"
+  },
+  "Image: Extract Max Representation": {
+    "resource_type": "image",
+    "url": "/build/code/static/img/extract_max.png"
+  },
+  "Image: Rebuild Heap Representation": {
+    "resource_type": "image",
+    "url": "/build/code/static/img/rebuild.png"
+  },  
+  "Image: Extract Min Representation": {
+    "resource_type": "image",
+    "url": "/build/code/static/img/Min-heap.png"
+  },
+  // Video artefacts
+
+  "Introduction Video": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/mo3sV66dxiQ"
+  },
+  "Insert Concept": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/KrLqtTkLVzo"
+  },
+  "Concept of Extract Min": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/yuf1CmCg13c"
+  },
+  "Concept of Heapsort": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/PSDztNb-b8k"
+  },
+};
